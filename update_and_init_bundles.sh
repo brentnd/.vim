2e2d801cee65438083810851d9d463499ca0e176
Bryan Paluch <bpaluch@zonoff.com>
2015-05-26 11:35:11 -0400

    add you complete me install stuff

--- update_and_init_bundles.sh
+++ update_and_init_bundles.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#update all the submodules in bundle
 git submodule init
 git submodule update
 git submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; git clean -dfx'
+
+
+
+#update and build YouCompletMe
+cd bundle/YouCompleteMe
+git submodule update --init --recursive
+./install.sh --clang-completer
+
